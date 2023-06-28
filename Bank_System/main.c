b48210840fd70657859c53dc2e5fe920ec88d28c
RBKNG7 <rubenasir7@gmail.com>
2023-06-28 18:30:09 +0200

    log in and authenticate function implemented

--- Bank_System/main.c
+++ Bank_System/main.c
@@ -10,56 +10,90 @@ typedef struct{
     int account_id;
     char password[10];
     int balance;
-} Cuenta;
+} Account;
 
-void crear_cuenta()
+void create_account()
 {
-    Cuenta cuenta;
+    Account account;
     getchar();
     puts("Introduzca su nombre y apellidos: ");
-    scanf("%[^\n]", cuenta.full_name);
+    scanf("%[^\n]", account.full_name);
     printf("Introduzca su DNI: ");
-    scanf("%s", cuenta.client_id);
+    scanf("%s", account.client_id);
     printf("Introduzca la contraseña: ");
-    scanf("%s", cuenta.password);
-    cuenta.balance = 0;
-    cuenta.account_id = rand() % 1000000;
+    scanf("%s", account.password);
+    account.balance = 0;
+    account.account_id = rand() % 1000000;
 
-    guardar_cuenta(&cuenta, cuenta.client_id);
+    guardar_cuenta(&account);
 }
 
-void guardar_cuenta(Cuenta c)
+void guardar_cuenta(Account c)
 {
     FILE *f;
     if(!(f = fopen("/home/ruby/programacion/c/BankManagement/database/informacion.dat", "ab"))){
         printf("No se ha podido abrir el fichero.");
         return 1;
     }
-    fwrite(&c, sizeof(Cuenta), 1, f);
+    fwrite(&c, sizeof(Account), 1, f);
     fclose(f);
 }
 
-void imprimir_cuenta(){
+void print_account(){
     FILE *f;
-    Cuenta cuenta;
+    Account account;
     if(!(f = fopen("/home/ruby/programacion/c/BankManagement/database/informacion.dat", "rb"))){
         printf("Error al abrir el archivo\n");
         return 2;
     }
-    while (fread(&cuenta, sizeof(Cuenta), 1, f)) {
-        printf("Nombre: %s\n", cuenta.full_name);
-        printf("DNI: %s\n", cuenta.client_id);
-        printf("ID de cuenta: %d\n", cuenta.account_id);
-        printf("Saldo: %d\n", cuenta.balance);
+    while (fread(&account, sizeof(Account), 1, f)) {
+        printf("Nombre: %s\n", account.full_name);
+        printf("DNI: %s\n", account.client_id);
+        printf("ID de cuenta: %d\n", account.account_id);
+        printf("Saldo: %d\n", account.balance);
         printf("\n");
     }
     fclose(f);
+}
 
+int login()
+{
+    char DNI[9];
+    char password[10];
+    printf("Introduzca su ID(DNI): ");
+    scanf("%s", DNI);
+    printf("Introduzca su contraseña: ");
+    scanf("%s", password);
+    if(authenticate(DNI, password)){
+        printf("El usuario %d ha iniciado sesion exitosamente", DNI);
+    }
+    else
+        printf("Usted no está registrado como cliente.");
+}
+
+int authenticate(char *id, char *password) {
+    FILE *f;
+    Account account;
+    if(!(f = fopen("/home/ruby/programacion/c/BankManagement/database/informacion.dat", "rb"))){
+        printf("Error al abrir el archivo\n");
+        return 3;
+    }
+    while (fread(&account, sizeof(Account), 1, f)){
+        if (strcmp(account.client_id, id) == 0 && strcmp(account.password, password) == 0) {
+            fclose(f);
+            return 1;
+        }
+    }
+    fclose(f);
+    return 0;
 }
 
 void imprimir_menu()
 {
-    printf("1. Crear nueva cuenta\n");
+    printf("[1] Crear nueva cuenta\n");
+    printf("[2] Iniciar sesion\n");
+    printf("[3] Mostrar cuenta\n");
+    printf("[4] Salir\n");
     printf("Seleccione una opción: ");
 }
 
@@ -75,14 +109,16 @@ int main() {
 
         switch (opcion) {
             case 1:
-                crear_cuenta();
+                create_account();
                 break;
-            case 2: imprimir_cuenta();
+            case 2:
+                login();
                 break;
-            default:
-                printf("Escoja una opción válida\n");
+            case 3:
+                print_account();
+                break;
         }
-    } while (opcion != 3);
+    } while (opcion != 4);
 
     printf("Saliendo...");
     return 0;
